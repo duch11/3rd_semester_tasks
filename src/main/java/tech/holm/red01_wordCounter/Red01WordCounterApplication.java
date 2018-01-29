50b07b58db450454c8c8fdb3b24a92c4774ed7b1
Jonas Ladefoged Holm <jonas@holm.tech>
2018-01-29 17:21:45 +0100

    Save

---
+++ src/main/java/tech/holm/red01_wordCounter/Red01WordCounterApplication.java
@@ -0,0 +1,12 @@
+package tech.holm.red01_wordCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Red01WordCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Red01WordCounterApplication.class, args);
+	}
+}
---
+++ src/test/java/tech/holm/red01_wordCounter/Red01WordCounterApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.holm.red01_wordCounter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Red01WordCounterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
